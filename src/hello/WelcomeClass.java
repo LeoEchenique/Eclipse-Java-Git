0fe71c55f9c465ee6d90d6d0eca9885be9273e24
LeoEchenique <leo.echeni@gmail.com>
2023-01-27 17:33:39 -0300

    added a for loop and collection to show array on console

--- src/hello/WelcomeClass.java
+++ src/hello/WelcomeClass.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.Arrays;	// importing a collection of the framework that makes possible to show an array on the console
+							// Arrays.deepToString() -> This method is used when you have to two dimensional array. [[]]
 public class WelcomeClass {
 
 	public static void main(String[] args) {
@@ -8,6 +10,12 @@ public class WelcomeClass {
 		System.out.println("Hello wordl!");
 		System.out.println("Hello!");
 		
+		String name= "leo";
+		String[] nombres= {"maria", "Ernesto", "Nahuel", "Lucia"};	
+		for (int i = 0; i < nombres.length; i++) {
+		  System.out.println(nombres[i]);
+		}
+		 System.out.println(Arrays.toString(nombres));
 	}
 
 }
