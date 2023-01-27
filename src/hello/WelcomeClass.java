0d75405cb43a52b898fc428a12536d75906e7941
LeoEchenique <leo.echeni@gmail.com>
2023-01-27 19:30:00 -0300

    formatter

--- src/hello/WelcomeClass.java
+++ src/hello/WelcomeClass.java
@@ -1,21 +1,23 @@
 package hello;
 
-import java.util.Arrays;	// importing a collection of the framework that makes possible to show an array on the console
+import java.util.Arrays; // importing a collection of the framework that makes possible to show an array on the console
 							// Arrays.deepToString() -> This method is used when you have to two dimensional array. [[]]
+
 public class WelcomeClass {
 
 	public static void main(String[] args) {
-		// public static void main -> method of a class. tells the compiler what code must be run first 
-		
+		// public static void main -> method of a class. tells the compiler what code
+		// must be run first
 		System.out.println("Hello wordl!");
 		System.out.println("Hello!");
-		
-	
-		String[] nombres= {"maria", "Ernesto", "Nahuel", "Lucia"};	
+
+		String[] nombres = { "maria", "Ernesto", "Nahuel", "Lucia" };
 		for (int i = 0; i < nombres.length; i++) {
-		  System.out.println(nombres[i]);
+			System.out.println(nombres[i]);
 		}
-		 System.out.println(Arrays.toString(nombres));
+		System.out.println(nombres);
+		System.out.println(Arrays.toString(nombres));
+		
 	}
 
 }
