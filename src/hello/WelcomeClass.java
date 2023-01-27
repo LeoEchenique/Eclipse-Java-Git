87be40e718895f3fde32745b84ba9707a8c98f9c
LeoEchenique <leo.echeni@gmail.com>
2023-01-27 16:53:53 -0300

    Eclipse, java, on git.

---
+++ src/hello/WelcomeClass.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class WelcomeClass {
+
+	public static void main(String[] args) {
+		// public static void main -> method of a class. tells the compiler what code must be run first 
+		
+		System.out.println("Hello wordl!");
+		System.out.println("Hello!");
+		
+	}
+
+}
