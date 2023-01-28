7e0a68a3f66de3ee75d0dea662f2e8e8e6a08012
LeoEchenique <leo.echeni@gmail.com>
2023-01-28 20:45:36 -0300

    understanding imports of public class methods

--- src/hello/WelcomeClass.java
+++ src/hello/WelcomeClass.java
@@ -8,16 +8,20 @@ public class WelcomeClass {
 	public static void main(String[] args) {
 		// public static void main -> method of a class. tells the compiler what code
 		// must be run first
-		System.out.println("Hello wordl!");
-		System.out.println("Hello!");
+	
+		//System.out.println("Hello wordl!");
+		//System.out.println("Hello!");
 
 		String[] nombres = { "maria", "Ernesto", "Nahuel", "Lucia" };
 		for (int i = 0; i < nombres.length; i++) {
-			System.out.println(nombres[i]);
+			holaNombre(nombres[i]);
 		}
-		System.out.println(nombres);
+		// System.out.println(nombres);
 		System.out.println(Arrays.toString(nombres));
 		
 	}
-
+	
+	public static void holaNombre(String nombre) {
+		System.out.println("Hola " + nombre);
+	}
 }
---
+++ src/hello/testingFn.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class testingFn {
+
+	public static void main(String[] args) {
+		WelcomeClass.holaNombre("juan");
+
+	}
+
+}
