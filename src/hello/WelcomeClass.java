98a7f7d017de2c85c4a553b784a41a34dd12d8e8
LeoEchenique <leo.echeni@gmail.com>
2023-01-30 16:42:53 -0300

    Taking more notes

--- src/hello/WelcomeClass.java
+++ src/hello/WelcomeClass.java
@@ -2,6 +2,7 @@ package hello;
 
 import java.util.Arrays; // importing a collection of the framework that makes possible to show an array on the console
 							// Arrays.deepToString() -> This method is used when you have to two dimensional array. [[]]
+import java.util.Scanner;
 
 public class WelcomeClass {
 
@@ -11,17 +12,30 @@ public class WelcomeClass {
 	
 		//System.out.println("Hello wordl!");
 		//System.out.println("Hello!");
-
-		String[] nombres = { "maria", "Ernesto", "Nahuel", "Lucia" };
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("type your text here: ");
+		String input= scanner.nextLine();
+		System.out.println("this is what you've typed: " + input);
+		scanner.close();
+		
+		
+		String[] nombres = { "maria", "Ernesto", "Nahuel", "Lucia", input};
+		
 		for (int i = 0; i < nombres.length; i++) {
 			holaNombre(nombres[i]);
 		}
-		// System.out.println(nombres);
-		System.out.println(Arrays.toString(nombres));
+		 System.out.println(Arrays.toString(nombres));
+		 if(nombres.length > 3) System.out.println(nombres.length + " yei");
+		 else System.out.println("not enough length");
 		
 	}
 	
+	
 	public static void holaNombre(String nombre) {
 		System.out.println("Hola " + nombre);
+	}
+	
+	protected void holaClase() {
+		System.out.println("un hola protegido");
 	}
 }
--- src/hello/testingFn.java
+++ src/hello/testingFn.java
@@ -1,10 +1,14 @@
 package hello;
 
 public class testingFn {
-
+	
+	
+	static String[] nombres = { "maria", "Ernesto", "Nahuel", "Lucia" };
+	
+	
 	public static void main(String[] args) {
+		WelcomeClass welcome= new WelcomeClass();
 		WelcomeClass.holaNombre("juan");
-
+		welcome.holaClase();
 	}
-
 }
