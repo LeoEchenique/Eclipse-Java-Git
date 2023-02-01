8b5213ca07d469eef52bb2581931945a2ba1f8d1
LeoEchenique <leo.echeni@gmail.com>
2023-02-01 01:19:48 -0300

    OOP Java with Eclipse

--- src/carsOOP/Cars.java
+++ src/carsOOP/Cars.java
@@ -3,23 +3,36 @@ package carsOOP;
 
 public class Cars {
 	
-	int ruedas; 	// props que tiene un car
-	int largo;
+	private int ruedas; 	// it's private because all cars have 4 wheels. ENCAPSULATION
+	int largo;		// dynamic props, this is bad. because other class or block of code can change the value without a setter. but for learning purposes for now will be keep it this way
 	int ancho;
-	int motor;
+	private String color;
 	int peso;
-	
+	private int year;
 	
 	//Constructor - siempre tiene el mismo nombre que la clase
 	
-	public Cars(int ruedas, int largo, int ancho, int  motor, int peso) {
-		this.ruedas=ruedas;
+	public Cars( int largo, int ancho, String color, int peso, int year) {
+		this.ruedas= 4;
 		this.largo= largo;
 		this.ancho=ancho;
-		this.motor=motor;
+		this.color=color;
 		this.peso=peso;
+		this.year=year;
 		
 	}
 	
+	public String basic_info() {
+		return "This Vehicule has " + this.ruedas + " wheels and " + this.peso + "Kg of weight";
+	}
+	public void setColor(String color) {
+		this.color=color;
+	}
+	public void setYear(int year) {
+		this.year=year;
+	}
 
+	public String getYear() {
+		return "This Vehicule is a " + this.year + " model";
+	}
 }
--- src/carsOOP/Use_Car.java
+++ src/carsOOP/Use_Car.java
@@ -1,9 +1,14 @@
 package carsOOP;
 
 public class Use_Car {
-
 	public static void main(String[] args) {
-		Cars honda= new Cars(4,2500,666,9999,99);
+		Cars honda= new Cars(500, 110, "red", 5000, 1980);
+		System.out.println(honda.basic_info());
+		
+		System.out.println(honda.getYear());
+		honda.setYear(2015);
+		System.out.println(honda.getYear());
+		
+		System.out.println(honda.peso);
 	}
-
 }
