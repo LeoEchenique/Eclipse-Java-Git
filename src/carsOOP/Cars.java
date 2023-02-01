c21c84fe7355bfc79238613195b3a5c8f95a63a6
LeoEchenique <leo.echeni@gmail.com>
2023-01-31 22:15:37 -0300

    upcoming next: OOP

---
+++ src/carsOOP/Cars.java
@@ -0,0 +1,25 @@
+package carsOOP; 
+
+
+public class Cars {
+	
+	int ruedas; 	// props que tiene un car
+	int largo;
+	int ancho;
+	int motor;
+	int peso;
+	
+	
+	//Constructor - siempre tiene el mismo nombre que la clase
+	
+	public Cars(int ruedas, int largo, int ancho, int  motor, int peso) {
+		this.ruedas=ruedas;
+		this.largo= largo;
+		this.ancho=ancho;
+		this.motor=motor;
+		this.peso=peso;
+		
+	}
+	
+
+}
---
+++ src/carsOOP/Use_Car.java
@@ -0,0 +1,9 @@
+package carsOOP;
+
+public class Use_Car {
+
+	public static void main(String[] args) {
+		Cars honda= new Cars(4,2500,666,9999,99);
+	}
+
+}
