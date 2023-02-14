1f126bc1e6ce702c994cdf8ea2277ad585dcef40
LeoEchenique <leo.echeni@gmail.com>
2023-02-14 16:37:15 -0300

    Refactor, buffers

--- src/file_reader/ReadFile.java
+++ src/file_reader/ReadFile.java
@@ -1,5 +1,6 @@
 package file_reader;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,25 +8,25 @@ import java.io.IOException;
 
 public class ReadFile {
 
-	
 	public void readsFile(String path) {
-		try (FileReader tbRead= new FileReader(path)) {   
-			 // this stream needs to be closed, but in java7 came a new feature made it in the line above, it's a "Try with resources"
-			//  the instance is created inside the try statement, and it will be automatically closed at the end of the block, 
-			// regardless of whether an exception occurs or not. This simplifies the code and helps prevent resource leaks.
-			int unicodeChar= 0;
-			while(unicodeChar != -1) {
+		try (FileReader tbRead = new FileReader(path)) {
+
+			BufferedReader buff = new BufferedReader(tbRead);
+			// this stream needs to be closed, but in java7 came a new feature made it in
+			// the line above, it's a "Try with resources"
+			// the instance is created inside the try statement, and it will be
+			// automatically closed at the end of the block,
+			// regardless of whether an exception occurs or not. This simplifies the code
+			// and helps prevent resource leaks.
+			String line="";
+			while (line != null) {
+				line = buff.readLine();
+				if(line != null) System.out.println(line);
 				
-				unicodeChar= tbRead.read();
-				char letras= (char) unicodeChar;
-				System.out.print(letras);
 			}
-			
-			
-		} catch(IOException e){
-			System.out.println("error"); 
+		} catch (IOException e) {
+			System.out.println("error");
 		}
 	}
 
-	
 }
